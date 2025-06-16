1154ec9e4403b218d789f6e35a183487f2add060
frou01 <frou01stg44hurutaka@gmail.com>
2025-06-16 18:28:07 +0900

    ベースクラス書き込み

--- Packages/com.frou01.rbur/Runtime/StraightControlling/AbstractSignalTrasnport.cs
+++ Packages/com.frou01.rbur/Runtime/StraightControlling/AbstractSignalTrasnport.cs
@@ -6,50 +6,83 @@ using VRC.Udon;
 
 public class AbstractSignalTrasnport : UdonSharpBehaviour
 {
+    //Face true = +Z
+    //Face false = -Z
     //Reference
     //参照した前後方向のTransporterの各変数を配列保存して高速参照する
     //---------------------------------------------------------------------------------------------ReferBlock
-    [SerializeField]AbstractSignalTrasnport[] connectedTransporter = new AbstractSignalTrasnport[2];
+    [SerializeField]public AbstractSignalTrasnport[] connectedTransporter = new AbstractSignalTrasnport[2];
     int[] localConnectedTransporterID = new int[2];
     [UdonSynced] int[] syncedConnectedTransporterID = new int[2];
-    bool[][] connectedTransporter_comingUpdateFace = new bool[2][];
+    bool[][] connectedTransporter_UpdateComingFace = new bool[2][];
     public virtual void changeConnection(bool ConnectingFace, AbstractSignalTrasnport newConnection,bool FaceofNewConnection)
     {
         int id = ConnectingFace ? 1 : 0;
         connectedTransporter[id] = newConnection;
-        syncedConnectedTransporterID[id] = localConnectedTransporterID[id] = connectedTransporter[id].SignalTransporterID;
-        //store pointer connectedTransporter[id].comingUpdateFace;
-        connectedTransporter_comingUpdateFace[id] = connectedTransporter[id].comingUpdateFace;
-
-        connectedFace[id] = FaceofNewConnection;
+        if(newConnection)
+        {
+            syncedConnectedTransporterID[id] = localConnectedTransporterID[id] = connectedTransporter[id].SignalTransporterID;
+            //store pointer connectedTransporter[id].comingUpdateFace;
+            connectedTransporter_UpdateComingFace[id] = connectedTransporter[id].UpdateComingFace;
+            connectedFace[id] = FaceofNewConnection;
+        }
+        else
+        {
+            syncedConnectedTransporterID[id] = localConnectedTransporterID[id] = -1;
+        }
     }
     //---------------------------------------------------------------------------------------------ReferBlockEnd
 
-    bool[] connectedFace = new bool[2];
+    [SerializeField] public bool[] connectedFace = new bool[2];
 
     //Preset
     [SerializeField] AbstractSignalProcessor signalProcessor;
     public int SignalTransporterID;
+
+    bool alreadySignalUpdated;
     //Temporary
-    public bool[] comingUpdateFace = new bool[1];
+    public bool[] UpdateComingFace = new bool[1];
+
+    int counter;
 
     protected virtual void Start()
     {
-        
+    }
+
+    public void LateUpdate()
+    {
+        alreadySignalUpdated = false;
+        counter = 0;
     }
 
 
 
-    public virtual void UpdateSignal(bool f_r)
+    public virtual void TransportSignal(bool f_r)
     {
         int id = f_r?1:0;
-        //connectedTransporter[id].comingUpdateFace[0] = connectedFace[id];
-        connectedTransporter_comingUpdateFace[id][0] = connectedFace[id];
+        //connectedTransporter[id].UpdateComingFace[0] = connectedFace[id];
+        connectedTransporter_UpdateComingFace[id][0] = connectedFace[id];
         connectedTransporter[id].OnSpawn();
+        Debug.Log("UpdateSignal");
+    }
+
+    public virtual bool SignalUpdate()
+    {
+        return false;
     }
     public override void OnSpawn()
     {
+        counter++;
+        if(counter > 500)
+        {
+            Debug.LogError("Infinite loop or too long train!");
+            return;
+        }
         //sendCustomEventを使用せず、使わないだろうPublicAPIで代替してやる
-        UpdateSignal(!comingUpdateFace[0]);//来たのとは反対側へ信号を飛ばす
+        if (!alreadySignalUpdated && SignalUpdate())
+        {
+            TransportSignal(UpdateComingFace[0]);//自身が優先信号を持っている場合は打ち返す。既に自身の信号を発出済みの場合は無視する
+        }
+        TransportSignal(!UpdateComingFace[0]);//来たのとは反対側へ信号を飛ばす
     }
 }
--- Packages/com.frou01.rbur/editor/SignalTrasnport_onEditor.cs
+++ Packages/com.frou01.rbur/editor/SignalTrasnport_onEditor.cs
@@ -8,9 +8,29 @@ using UnityEngine.SceneManagement;
 public class SignalTrasnport_onEditor : IProcessSceneWithReport
 {
     public int callbackOrder => 0;
+    public List<AbstractSignalTrasnport> target = new List<AbstractSignalTrasnport>();
 
     public void OnProcessScene(Scene scene, BuildReport report)
     {
+        foreach (AbstractSignalTrasnport transporter in target)
+        {
+            if (transporter.connectedTransporter[0]) transporter.changeConnection(false, transporter.connectedTransporter[0], transporter.connectedFace[0]);
+            if (transporter.connectedTransporter[1]) transporter.changeConnection(true , transporter.connectedTransporter[1], transporter.connectedFace[1]);
+            //‰Šú‰»
+        }
         //AbstractSignalTrasnport‚ÌQÆ“¯Šú—p‚ÉSignalTrasnportManager‚ÉID‚ğ•Û‘¶
     }
+
+    void Proceed(Transform parent)
+    {
+        if (parent.gameObject.GetComponent<AbstractSignalTrasnport>() != null)
+        {
+            target.Add(parent.gameObject.GetComponent<AbstractSignalTrasnport>());
+        }
+        foreach (Transform obj in parent)
+        {
+            Proceed(obj);
+        }
+    }
+
 }
